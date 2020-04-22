2259c83051aa84da05b680b7189a4a553a233955
Kasel1997 <64045356+Kasel1997@users.noreply.github.com>
2020-04-22 17:02:04 +1000

    Update program.cpp
    Update

--- program.cpp
+++ program.cpp
@@ -5,7 +5,7 @@ int main()
 {
     string name;
     cout << "Hello" << endl;
-    cout << "two" << endl;
+    cout << "test test" << endl;
     cout << "what is your age" << endl;
     cin >> name;
 
